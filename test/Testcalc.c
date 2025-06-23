57f5a2460e4f3613818bdc4c7f89f46765dd3025
juHackWeg <justin-lind@web.de>
2025-06-23 18:36:40 +0200

    ADD basic unity test setup

---
+++ test/Testcalc.c
@@ -0,0 +1,26 @@
+#include "unity.h"
+#include "calc.h"
+
+void	setUp()
+{
+}
+
+void	tearDown()
+{
+}
+
+void	test_add()
+{
+	int	output;
+
+	output = add(10, 20);
+	TEST_ASSERT_EQUAL(30, output);
+}
+
+int	main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(test_add);
+	UNITY_END();
+	return (0);
+}
