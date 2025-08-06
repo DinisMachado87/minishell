05aca69e52178a5ad68065770083593dcd2e4866
dimachad <dinisma@gmail.com>
2025-08-06 15:38:00 +0200

    Rebase extract_command

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dimachad <dimachad@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 15:31:32 by dimachad          #+#    #+#             */
-/*   Updated: 2025/08/06 15:30:31 by dimachad         ###   ########.fr       */
+/*   Updated: 2025/08/06 15:36:44 by dimachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,25 +27,48 @@
 # include <sys/wait.h>
 # include <stddef.h>
 
+typedef enum {
+	CMD,
+	PIPE,
+	REDIRECT_OUT,
+	REDIRECT_IN,
+	SUBSHELL,
+	OR,
+	AND,
+	APPEND,
+	HEREDOC,
+} t_type;
+
 typedef struct s_ast
 {
 	int				type;
 	int				subtype_maybe;
 	char			**args;
+	int				n_args;
 	struct s_ast	*next;
 	struct s_ast	*left;
 	struct s_ast	*right;
 }					t_ast;
 
-typedef struct	s_state_ext_cmd
+typedef struct	s_state_parser
 {
 	char	*str;
+	int		needle;
 	int 	i_word;
 	int		error;
-}				t_s_ext_cmd;
+}				t_s_parser;
 
-int	make_node(t_ast **ast);
-int	extract_cmd(t_ast *ast, char *str);
 void	prompt_loop(void);
+// ast_utils
+t_ast	*make_node(t_ast **ast);
+t_ast	*free_all(t_ast *ast);
+// parser_utils
+int		is_operator(char *str);
+char	*ms_strcpy(char *str, int len);
+// gen_utils
+void	ms_bzero(void *s, size_t n);
+// extract cmd
+t_ast	*extract_cmd(char **str, t_s_parser *s);
+// int		split_parser(t_ast *ast, char *str, int *error);
 
 #endif
