e316a4d2584df534199949ea66c069486c25f582
jlind <justin-lind@web.de>
2025-08-12 22:18:30 +0200

    ADD second handler depending on whether rdline active or not

--- src/helper/loop.c
+++ src/helper/loop.c
@@ -3,35 +3,56 @@
 void	sig_c_handler(int sig)
 {
 	(void)sig;
+	write(STDOUT_FILENO, "\n", 1);
+}
+
+void	sig_c_rdline_handler(int sig)
+{
+	(void)sig;
 	rl_replace_line("", 0);
 	write(STDOUT_FILENO, "\n", 1);
 	rl_on_new_line();
 	rl_redisplay();
 }
 
+void	set_handler(int rdline)
+{
+	struct sigaction	sa_c;
+
+	if (rdline)
+		sa_c.sa_flags = SA_RESTART;
+	sigemptyset(&sa_c.sa_mask);
+	sa_c.sa_handler = sig_c_handler;
+	if (rdline)
+		sa_c.sa_handler = sig_c_rdline_handler;
+	if (sigaction(SIGINT, &sa_c, NULL) == -1)
+		perror("sigaction");
+}
+
+
 char  *get_prompt(void)
 {
-  char  *cwd;
-  char  *prompt;
-  char	*user;
+	char  *cwd;
+	char  *prompt;
+	char	*user;
 
-  cwd = getcwd(NULL, 0);
-  user = getenv("USER");
-  if (!cwd)
-    return (NULL);
-  prompt = malloc((strlen(cwd) + strlen(user) + 6));
-  if (!prompt)
-  {
-    free(cwd);
-    return (NULL);
-  }
-  prompt[0] = '\0';
-  strncat(prompt, user, strlen(user));
-  strncat(prompt, ":", 2);
-  strncat(prompt, cwd, strlen(cwd));
-  strncat(prompt, "$ ", 3);
-  free(cwd);
-  return (prompt);
+	cwd = getcwd(NULL, 0);
+	user = getenv("USER");
+	if (!cwd)
+	  return (NULL);
+	prompt = malloc((strlen(cwd) + strlen(user) + 6));
+	if (!prompt)
+	{
+	  free(cwd);
+	  return (NULL);
+	}
+	prompt[0] = '\0';
+	strncat(prompt, user, strlen(user));
+	strncat(prompt, ":", 2);
+	strncat(prompt, cwd, strlen(cwd));
+	strncat(prompt, "$ ", 3);
+	free(cwd);
+	return (prompt);
 }
 
 /*
@@ -39,18 +60,19 @@ char  *get_prompt(void)
  */
 char  *get_input(char *prompt)
 {
-  char  *input;
+	char  *input;
 
-	// Maybe here we have to set the global variable that the main loop has to check on
-  input = readline(prompt);
-  if (!input)
+	set_handler(1);
+	input = readline(prompt);
+	set_handler(0);
+	if (!input)
 	{
 		free(prompt);
-    exit(1);
+		exit(1);
 	}
-  if (*input)
-    add_history(input);
-  return (input);
+	if (*input)
+		add_history(input);
+	return (input);
 }
 
 /*
@@ -61,21 +83,14 @@ void	prompt_loop(void)
 {
 	char  *input;
 	char  *prompt;
-	struct sigaction	sa_c;
 	t_shell	shell;
 
 	ms_bzero((void *)&shell, sizeof(t_shell));
 	shell.env = init_env();
-	//set_env_node(&shell.env, "PATH", "/usr/bin");
-
-  while (1)
+	set_handler(0);
+	signal(SIGQUIT, SIG_IGN);
+	while (1)
 	{
-		sa_c.sa_flags = SA_RESTART;
-		sigemptyset(&sa_c.sa_mask);
-		sa_c.sa_handler = sig_c_handler;
-		if (sigaction(SIGINT, &sa_c, NULL) == -1)
-			perror("sigaction");
-		signal(SIGQUIT, SIG_IGN);
 		prompt = get_prompt();
 		if (!prompt)
 			return ;
@@ -98,4 +113,3 @@ void	prompt_loop(void)
 		free(input);
 	free(prompt);
 }
-
