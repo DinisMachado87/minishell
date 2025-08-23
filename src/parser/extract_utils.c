7c0ed7910f24a59e6bafed70d6d0e43e75da0536
dimachad <dinisma@gmail.com>
2025-08-23 22:43:45 +0200

    Merge to debug allocate ast args

--- src/parser/extract_utils.c
+++ src/parser/extract_utils.c
@@ -6,31 +6,27 @@
 /*   By: dimachad <dimachad@student.42berlin.d>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 14:36:48 by dimachad          #+#    #+#             */
-/*   Updated: 2025/08/23 21:23:25 by dimachad         ###   ########.fr       */
+/*   Updated: 2025/08/06 14:46:28 by dimachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include <strings.h>
 
-static int	allocate_and_null(void *ptr, int size, char *error_str)
-{
-	ptr = malloc(size);
-	if (ptr)
-		return (perror(error_str), 0);
-	ms_bzero(ptr, size);
-	return (1);
-}
-
 int	allocate_ast_args(t_ast *ast, int n_strs)
 {
-	char error_str[] = "Err allocating args array";
-	int size;
-
-	size = n_strs + 1;
-	if (!allocate_and_null(ast->args, size * sizeof(char *), error_str)
-		|| !allocate_and_null(ast->exp_args, size * sizeof(int), error_str)
-		|| !allocate_and_null(ast->space_args, size * sizeof(int), error_str))
+	ast->args = malloc((n_strs + 1) * sizeof(char *));
+	if (!ast->args)
+		return (perror("Err allocating args array"), 0);
+	ast->exp_args = malloc((n_strs + 1) * sizeof(int));
+	if (!ast->exp_args)
+		return (perror("Err allocating args array"), 0);
+	ast->space_args = malloc((n_strs + 1) * sizeof(int));
+	if (!ast->space_args)
+		return (perror("Err allocating args array"), 0);
+	ms_bzero(ast->args, (n_strs + 1) * sizeof(char *));
+	ms_bzero(ast->exp_args, (n_strs + 1) * sizeof(int));
+	ms_bzero(ast->space_args, (n_strs + 1) * sizeof(int));
 	ast->n_args = n_strs;
 	return (1);
 }
