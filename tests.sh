21f4a7272424b4cda8d5358557fdc3be005eb8ed
Dinis Machado <dimachad@student.42berlin.de>
2025-10-06 17:05:23 +0200

    Delete support files for eval

--- tests.sh
+++
@@ -1,9 +0,0 @@
-export env=" doesn't trust my words" && echo justin"$env"! && echo "\n" && echo '\n' && echo \n
-export env='ls -l' && $env
-export env=justin && echo -n dinis'&&'$env"Minishell"
-export env=hello && echo $e
-export env=hello && echo $en
-export env=hello && echo $env
-export env2=hi && echo $e
-echo $? hello
-echo $?hello
