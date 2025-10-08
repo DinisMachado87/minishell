5e57c89f586e85e2dde2671ea664c35557be12f5
Justin Lind <jlind@student.42berlin.de>
2025-10-08 11:57:32 +0200

    MOD improved error message & general print of 'exit'

--- src/builtins/ms_exit.c
+++ src/builtins/ms_exit.c
@@ -6,7 +6,7 @@
 /*   By: jlind <jlind@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 09:31:33 by jlind             #+#    #+#             */
-/*   Updated: 2025/10/01 09:31:39 by jlind            ###   ########.fr       */
+/*   Updated: 2025/10/08 11:56:07 by jlind            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	ms_exit(t_shell *shell, t_ast *node)
 {
 	int	exit_status;
 
+	write(2, "exit\n", 5);
 	if (node->args[0].n > 2)
 		exit_status = print_err("exit", "too many arguments", 1);
 	else if (node->args[0].tkns[1])
@@ -23,7 +24,8 @@ void	ms_exit(t_shell *shell, t_ast *node)
 		if (ms_isdigit(node->args[0].tkns[1]))
 			exit_status = ms_atoi(node->args[0].tkns[1]);
 		else
-			exit_status = print_err("exit", "numeric argument required", 2);
+			exit_status = print_sys_err("exit", node->args[0].tkns[1],
+					"numeric argument required", 2);
 	}
 	else
 		exit_status = shell->exit_status;
